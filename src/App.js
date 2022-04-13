5680fb38ebb8b0ae2db70ce7af0e94bdad77bfd3
H1DER <artemohotnik2006@gmail.com>
2022-04-13 14:02:16 +0300

    ready

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component, useState  } from 'react'
+import generator from "generate-password";
+import {render} from "react-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App()  {
+  const [password, setPassword] = useState('Here will be your password');
+    const [isSymbols, setIsSymbols] = useState(false);
+
+    function fun1(){
+        var rng = document.getElementById('r1'); //rng - это ползунок
+        var p=document.getElementById('one'); // p - абзац
+        p.innerHTML=rng.value;
+        length = rng.value;
+
+    }
+
+  const createPassword = () => {
+
+      const pwd = generator.generate({
+
+          length: length,
+          lowercase: true,
+          uppercase: true,
+          numbers: true,
+          symbols: isSymbols,
+    });
+    setPassword(pwd);
+    console.log(password);
+    console.log(length);
+  }
+
+  const savePassword = () => {
+
+  }
+
+
+    return  (
+
+        <div>
+            <p className="title">New Password</p>
+           <div className="main__inter">
+          <div className="password__style">
+            <p>{password}</p>
+          </div>
+               <div className="input">
+                   <input min="5" max="25" type="range" id="r1" onChange={fun1}/>
+                   <p id="one">{length}</p>
+               </div>
+
+          <button onClick={createPassword} className="button">Create</button>
+
+           </div>
+</div>
+    )
+
 }
+
+
 
 export default App;
